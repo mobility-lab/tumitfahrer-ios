a1c818a47d2ff219a782034c40055ae30168664c
Daniel Böning <danielboning@dhcp-10-180-44-148.dynamic.eduroam.mwn.de>
2015-09-23 12:41:49 +0200

    Change Header for Open Source

--- tumitfahrer/StomtMapping.h
+++ tumitfahrer/StomtMapping.h
@@ -2,8 +2,21 @@
 //  StomtMapping.h
 //  tumitfahrer
 //
-//  Created by Pawel Kwiecien on 7/26/14.
-//  Copyright (c) 2014 Pawel Kwiecien. All rights reserved.
+/*
+ * Copyright 2015 TUM Technische Universität München
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 //
 
 #import <Foundation/Foundation.h>
